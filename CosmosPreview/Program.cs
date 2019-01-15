c27e7b450f8cf65a301334e92ff856b36d1485d2
Kevin Meyer <//>
2019-01-15 14:29:01 +0100

    async main

--- CosmosPreview/Program.cs
+++ CosmosPreview/Program.cs
@@ -1,24 +1,25 @@
 ﻿using CosmosPreview.Models;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CosmosPreview
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using (var context = new SchroedingerContext())
             {
-                context.Database.EnsureCreated();
+                await context.Database.EnsureCreatedAsync();
 
                 var cat = new Cat { CatId = 1, IsAlive = false };
                 var box = new Box { BoxId = 1, Cat = cat };
 
-                context.Boxes.Add(box);
+                await context.Boxes.AddAsync(box);
 
                 //Persistiert alle Änderungen im Context
-                context.SaveChanges();
+                await context.SaveChangesAsync();
 
                 var remoteBox = context.Boxes.First();
                 Console.WriteLine(remoteBox.ToString());
